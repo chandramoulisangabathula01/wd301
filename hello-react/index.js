078411a4734a9a9f68e4de253f3c92bf7a4d143d
chandramoulisangabathula01 <chandramoulisangabathula01@gmail.com>
2024-03-07 00:43:55 +0530

    added typescript

---
+++ hello-react/index.js
@@ -0,0 +1,14 @@
+// interface User {
+//     name : string;
+//     id:number;
+//     isAdmin:boolean;
+// }
+// let newUser: User = {
+//     name: "javaScript",
+//     id:2,
+//     isAdmin:false
+// }
+// let myNumber:number;
+// myNumber = 5 ;
+// console.log(myNumber);
+
