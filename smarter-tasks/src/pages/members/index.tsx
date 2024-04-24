3fd4dc77fc96931c2c3b6fd002da34191fe6a161
chandramoulisangabathula01 <chandramoulisangabathula01@gmail.com>
2024-04-24 14:49:39 +0530

    level 7 try 2  newmembers and memberslist fixed

--- smarter-tasks/src/pages/members/index.tsx
+++ smarter-tasks/src/pages/members/index.tsx
@@ -1,6 +1,19 @@
+import MemberList from "./MemberList";
+import NewMember from "./NewMember";
+
 const Members = () => {
     return (
-      <h2>Members</h2>
+      
+      <>
+        <div className="flex justigy-between bg-grey-900">
+
+        <h2 className="opacity-15 text-2xl font-medium tracking-normal" >Members</h2>
+        <NewMember />
+
+        </div>
+        <MemberList />
+      </>
+      
     )
   }
   export default Members;
